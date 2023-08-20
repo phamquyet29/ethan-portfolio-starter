d7a01f298ebef511b6a38e250a3897fce1653a32
quyet <phamquyet3377@gmail.com>
2023-08-20 23:45:53 +0700

    portfolio

--- components/Socials.js
+++ components/Socials.js
@@ -1,5 +1,31 @@
+//links
+import Link from "next/link";
+//icons
+import { RiInstagramLine, RiGithubLine, RiFacebookLine } from "react-icons/ri";
+
 const Socials = () => {
-  return <div>Socials</div>;
+  return (
+    <div className="flex items-center gap-x-5 text-lg">
+      <Link
+        href={"https://www.facebook.com/profile.php?id=100050045045324"}
+        className="hover:text-accent transition-all duration-300"
+      >
+        <RiFacebookLine />
+      </Link>
+      <Link
+        href={"https://www.instagram.com/quyetnong7/"}
+        className="hover:text-accent transition-all duration-300"
+      >
+        <RiInstagramLine />
+      </Link>
+      <Link
+        href={"https://github.com/phamquyet29"}
+        className="hover:text-accent transition-all duration-300"
+      >
+        <RiGithubLine />
+      </Link>
+    </div>
+  );
 };
 
 export default Socials;
--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,5 +1,68 @@
+//components
+import Circles from "/components/Circles";
+
+//icons
+import { BsArrowRight } from "react-icons/bs";
+//framer
+import { motion } from "framer-motion";
+//variants
+import { fadeIn } from "../../variants";
+
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div className="h-full bg-primary/30">
+      <div className="container mx-auto py-32 text-center xl:text-left flex justify-center items-center h-full">
+        {/* text & from */}
+        <div className="flex flex-col w-full max-w-[700px]">
+          {/* text */}
+          <motion.h2
+            variants={fadeIn("up", 0.2)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="h2 text-center mb-12"
+          >
+            Contact <span className="text-accent">me.</span>
+          </motion.h2>
+          {/* from */}
+          <motion.form
+            variants={fadeIn("up", 0.4)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="flex-1 flex flex-col gap-6 w-full mx-auto"
+          >
+            {/*input group */}
+            <div className="flex gap-x-6 w-full">
+              <input type="text" placeholder="name" className="input" />
+              <input type="text" placeholder="email" className="input" />
+            </div>
+            <input type="text" placeholder="subject" className="input" />
+            <textarea placeholder="message" className="textarea"></textarea>
+            <button className="btn rounded-full border border-white/50 max-w-[170px] px-8 transition-all duration-300 flex items-center justify-center overflow-hidden hover:border-accent group">
+              <span className="group-hover:-translate-y-[120%] group-hover:opacity-0 translate-all duration-500">
+                Send
+              </span>
+              <BsArrowRight className="-translate-y-[120%] opacity-0 group-hover:flex group-hover:-translate-y-0 group-hover:opacity-100 transition-all duration-300 absolute text-[22px]" />
+            </button>
+          </motion.form>
+          <div>
+            <motion.p
+              variants={fadeIn("up", 0.2)}
+              initial="hidden"
+              animate="show"
+              exit="hidden"
+              className=" text-center mb-12"
+            >
+              TelePhone : <span className="text-accent"> 0789257816</span>
+              <br />
+              Email :<span className="text-accent"> phamquyet3377@gmail.com</span>
+            </motion.p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
