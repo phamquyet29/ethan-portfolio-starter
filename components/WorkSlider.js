6d9c9ada7836d1569b8a04fdd87113803eff5d86
quyet <phamquyet3377@gmail.com>
2023-08-21 14:16:39 +0700

    update

--- components/WorkSlider.js
+++ components/WorkSlider.js
@@ -9,6 +9,7 @@ import Image from "next/image";
 import { Pagination } from "swiper";
 // icons
 import { BsArrowRight } from "react-icons/bs";
+import Link from "next/link";
 
 // data
 const workSlides = {
@@ -17,39 +18,19 @@ const workSlides = {
       images: [
         {
           title: "title",
-          path: "/thumb1.jpg",
+          path1: "/thumb1.jpg",
         },
         {
           title: "title",
-          path: "/thumb2.jpg",
+          path2: "/thumb2.jpg",
         },
         {
           title: "title",
-          path: "/thumb3.jpg",
+          path3: "/thumb3.jpg",
         },
         {
           title: "title",
-          path: "/thumb4.jpg",
-        },
-      ],
-    },
-    {
-      images: [
-        {
-          title: "title",
-          path: "/thumb4.jpg",
-        },
-        {
-          title: "title",
-          path: "/thumb1.jpg",
-        },
-        {
-          title: "title",
-          path: "/thumb2.jpg",
-        },
-        {
-          title: "title",
-          path: "/thumb3.jpg",
+          path4: "/thumb4.jpg",
         },
       ],
     },
@@ -78,23 +59,53 @@ const WorkSlider = () => {
                   >
                     <div className="flex items-center justify-center relative overflow-hidden group">
                       {/* image */}
-                      <Image src={image.path} width={500} height={300} alt="" />
+                      <Image
+                        src={image.path1}
+                        width={500}
+                        height={300}
+                        alt=""
+                      />
+                      <Image
+                        src={image.path2}
+                        width={500}
+                        height={300}
+                        alt=""
+                      />
+
+                      <Image
+                        src={image.path3}
+                        width={500}
+                        height={300}
+                        alt=""
+                      />
+                      <Image
+                        src={image.path4}
+                        width={500}
+                        height={300}
+                        alt=""
+                      />
+
                       {/* overlay gradient */}
+
                       <div className="absolute inset-0 bg-gradient-to-l from-transparent via-[#e838cc] to-[#4a22bd] opacity-0 group-hover:opacity-80 transition-all duration-700"></div>
+
                       {/* title */}
+
                       <div className="absolute bottom-0 translate-y-full group-hover:-translate-y-10 group-hover:xl:-translate-y-20 transition-all duration-300">
-                        <div className="flex items-center gap-x-2 text-[13px] tracking-[0.2em]">
-                          {/* title part 1 */}
-                          <div className="delay-100">LIVE</div>
-                          {/* title part 2 */}
-                          <div className="translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-150">
-                            PROJECT
+                        <Link href={"https://github.com/HuTao2k3/da1_nhom2"}>
+                          <div className="flex items-center gap-x-2 text-[13px] tracking-[0.2em]">
+                            {/* title part 1 */}
+                            <div className="delay-100">LIVE</div>
+                            {/* title part 2 */}
+                            <div className="translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-150">
+                              PROJECT
+                            </div>
+                            {/* icon */}
+                            <div className="text-xl translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-200">
+                              <BsArrowRight />
+                            </div>
                           </div>
-                          {/* icon */}
-                          <div className="text-xl translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-200">
-                            <BsArrowRight />
-                          </div>
-                        </div>
+                        </Link>
                       </div>
                     </div>
                   </div>
